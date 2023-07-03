526e6da08b4bc51e926cb5a8c2f115c1dc0d3a1f
Brian Miranda <brianmiranda.dev@gmail.com>
2023-07-03 00:07:45 -0400

    48. Creating custom buttons

--- src/screens/StartGame/index.jsx
+++ src/screens/StartGame/index.jsx
@@ -1,9 +1,12 @@
 import { View, TextInput } from 'react-native';
+import PrimaryButton from '../../components/PrimaryButton';
 
 const StartGame = () => {
   return (
     <View>
       <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
